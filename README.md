d03198816f18b1e9c5e5006ee24d363b2b9a66a4
Ashley-Blu <matsekolengashley28@hmail.com>
2025-07-24 06:30:31 +0200

    Created a hello script and a Readme file

---
+++ README.md
@@ -0,0 +1,1 @@
+![alt](assets/hero.png)
---
+++ hello.py
@@ -0,0 +1,5 @@
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name}. Welcome to the our python program.")
